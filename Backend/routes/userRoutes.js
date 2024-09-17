e6ff975ad20a22cd71f0fe2991385b33f4487f75
Arqam-Siddiqi <siddiqi.seerow@gmail.com>
2024-09-18 00:44:49 +0500

    Added OAuth2.0

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,33 +1,10 @@
 const express = require('express');
-const query = require('../database/psqlWrapper')
+const userController = require('../controllers/userController');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    try{
-        const users = await query('SELECT * FROM Users');
-        
-        res.status(200).send(users.rows);
-    }   
-    catch(error){
-        res.status(400).send(error.message);
-    }
-});
+router.get('/', userController.getAllUsers);
 
-router.post('/create', async (req, res) => {
-
-    try{
-        const user = await query(
-            'INSERT INTO Users (name) VALUES ($1) RETURNING *',
-            [req.body.name]
-        );
-
-        res.send(user.rows[0]);
-    }
-    catch(error){
-        res.send(error.message);
-    }
-
-});
+router.post('/create', userController.createUser);
 
 module.exports = router;
