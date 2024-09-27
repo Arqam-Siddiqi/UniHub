a8e9eabe3189f08e96b8062f5c6b0195f77fd575
Arqam-Siddiqi <siddiqi.seerow@gmail.com>
2024-09-27 21:52:13 +0500

    Added googleVerified param

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -13,7 +13,7 @@ router.get('/google/callback', passport.authenticate('google'), (req, res) => {
     const token = jwt.sign({ id: req.user.id }, process.env.JWT_SECRET, { expiresIn: '3d' })
     
     res.cookie('jwtToken', token);
-    res.status(200).send({user: req.user.name, jwt: token});
+    res.status(200).send({user: req.user.name, jwt: token, googleVerified: req.user.google_id ? true : false});
 })
 
 module.exports = router;
