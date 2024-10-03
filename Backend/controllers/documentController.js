f2083ab2bb3a29ede9559718a490108d7ba4bb1f
Arqam-Siddiqi <siddiqi.seerow@gmail.com>
2024-10-04 01:34:59 +0500

    Changes for Render.

--- Backend/controllers/documentController.js
+++ Backend/controllers/documentController.js
@@ -1,7 +1,12 @@
 const path = require('path');
 const multer = require('multer');
 
-const storage = multer.memoryStorage();
+const storage = multer.diskStorage({
+  destination: './downloads/',
+  filename: (req, file, cb) => {
+    cb(null, `${file.originalname}`);
+  }
+});
 
 const upload = multer({
   storage: storage,
@@ -12,22 +17,40 @@ const upload = multer({
 }).single('document');
 
 
-const uploadFile = (req, res) => {
+const uploadFile = async (req, res) => {
 
     try{
-        upload(req, res, (err) => {
-          
+        await upload(req, res, (err) => {
+            console.log(req.file);
             if(err || !req.file){
                 return res.status(400).send({"Error": err});
             }
     
-            console.log(req.file);
             res.status(200).send({"Message": "File uploaded."});
         });
     }
     catch(error){
         res.status(400).send({"Error": error.message});
     }
+}
+
+const downloadFile = (req, res) => {
+
+  try{
+    const filePath = './downloads/' + 'APznzaZfTXy8AzjdtzpkMg-0O4meK62R8nVED-5LuM3pbkrAXl42Cd1xV0LjsVmIYVQSVr7hpJYELcVY8m-aI1pB_Zr2JsI-y9Ce0ymXm1XEnYNq91qJMZ-c8hf96Njq0kH22zF-4LQ9NdDLLsjr5G-rI2dJROtPa7Xtc5hsvwH6UO1YAS_kYANF_cq0V3DecJ5LQPy8C8ocSUoH5nYXPXYoO5ZcU2.pdf'
+
+    res.download(filePath, (err) => {
+      if (err) {
+        console.error(err);
+        res.status(404).send('File not found');
+      }
+    });
+
+  }
+  catch(error){
+    res.status(400).send({"Error": error.message});
+  }
+
 }
 
 // Allowed extensions
@@ -46,5 +69,6 @@ function checkFileType(file, cb) {
   }
 
 module.exports = {
-    uploadFile
+    uploadFile,
+    downloadFile
 }
--- Backend/routes/documentRoutes.js
+++ Backend/routes/documentRoutes.js
@@ -4,4 +4,6 @@ const documentController = require('../controllers/documentController');
 
 router.post('/upload', documentController.uploadFile);
 
+router.get('/download', documentController.downloadFile);
+
 module.exports = router;
