15a02d45bd462f287a3071a8474c5d93972405fe
Arqam-Siddiqi <siddiqi.seerow@gmail.com>
2024-10-04 23:32:17 +0500

    Added multiple origins.

--- Backend/app.js
+++ Backend/app.js
@@ -14,20 +14,7 @@ const documentRoutes = require('./routes/documentRoutes');
 
 const app = express();
 
-const allowedOrigins = ['http://localhost:5713', 'http://localhost:3000', 'https://unihub-frontend.vercel.app'];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true, // If you want to include cookies in the request
-}));
+app.use(cors({ origin: [process.env.FRONTEND, 'http://localhost:5173', 'http://localhost:3000'], credentials: true }));
 
 setup()
   .then(() => {
