d384deaca1c58171efc2075ebc29c41f54a5e0f1
SaadAliBaig84 <mirzasaad363@gmail.com>
2024-10-09 15:39:25 -0700

    repo done

--- Backend/controllers/repoController.js
+++ Backend/controllers/repoController.js
@@ -18,7 +18,7 @@ const createRepo = async (req, res) => {
 
     try{
         const user_id = req.user;
-        const validated_params = validateRepoParams(user_id, req.body);
+        const validated_params = validateRepoParams(user_id, req.body, 'c');
 
         const repos = await repoQuery.queryAllRepos();
         const names = repos.map(data => data.name);
@@ -27,7 +27,7 @@ const createRepo = async (req, res) => {
             throw Error(`This User alreay has a repository named \"${validated_params.name}\".`);
         }
 
-        const repo = await repoQuery.createRepo(validated_params);
+        const repo = await repoQuery.createRepo(user_id,validated_params);
 
         res.status(200).send(repo);
     }
@@ -51,8 +51,51 @@ const getReposByJWT = async (req, res) => {
 
 }
 
+const updateRepo =async (req, res)=>{
+    try {
+        const user_id=req.user;
+        const validated_params = validateRepoParams(user_id, req.body, 'u');
+        const repo = await repoQuery.queryAllRepos();
+        const ids = repo.map(data => data.id);
+        const names = repo.map(data=>data.name);
+        const currName=await repoQuery.queryRepoNameOfUser( req.body.id);
+        if(!ids.includes(req.body.id)){
+            throw Error(`There is no repo with id  "${req.body.id}".`);
+        }
+        if(currName[0].name!==req.body.name){
+            if(names.includes(req.body.name) ){
+                throw Error(`There is already a repo named "${req.body.name}".`)
+            }
+        }
+       
+        
+        const repos= await repoQuery.update(validated_params);
+        res.status(200).send(repos);
+    } catch (error) {
+        res.status(400).send({"Error": error.message});
+    }
+}
+
+const deleteRepo = async (req, res)=>{
+    try {
+        const repo = await repoQuery.queryAllRepos();
+        const ids = repo.map(data => data.id);
+        const user_id=req.user;
+        const validated_params = validateRepoParams(user_id,req.body, 'd');
+        if(!ids.includes(req.body.id)){
+            throw Error(`There is no repo with id  "${req.body.id}".`);
+        }
+        const repos=await repoQuery._delete(validated_params);
+        res.status(200).send(repos);
+    } catch (error) {
+        res.status(400).send({"Error": error.message});
+    }
+}
+
 module.exports = {
     getAllRepos,
     createRepo,
-    getReposByJWT
+    getReposByJWT,
+    updateRepo,
+    deleteRepo
 }
--- Backend/database/repoQuery.js
+++ Backend/database/repoQuery.js
@@ -1,3 +1,4 @@
+const { user } = require('pg/lib/defaults');
 const {query} = require('./psqlWrapper');
 
 const queryAllRepos = async () => {
@@ -5,19 +6,19 @@ const queryAllRepos = async () => {
     const repos = await query(`
         SELECT * FROM Repos;   
     `);
-
+    console.log(repos.rows);
     return repos.rows;
 
 }
 
-const createRepo = async ({name, user_id, visibility, description}) => {
+const createRepo = async (user_id,{name,  description, visibility,}) => {
 
     const repo = await query(`
         INSERT INTO Repos (name, user_id, visibility, description) VALUES
         ($1, $2, $3, $4)
         RETURNING *;
     `, [name, user_id, visibility, description]);
-
+    console.log(repo.rows[0]);
     return repo.rows[0];
 
 }
@@ -29,12 +30,48 @@ const queryAllReposOfUser = async (user_id) => {
         WHERE user_id = $1    
     `, [user_id]);
 
-
+    console.log(repos.rows);
     return repos.rows;
+}
+
+const queryRepoNameOfUser = async (id)=>{
+    const repos = await query(`
+        SELECT name FROM Repos
+        WHERE id = $1   
+    `, [id]);
+
+    console.log(repos.rows);
+    return repos.rows;
+}
+
+const update = async ({id,name, description, visibility})=>{
+    const repos= await query(
+        `UPDATE Repos
+        SET name = COALESCE($2, name),
+            description = COALESCE($3, description),
+            visibility = COALESCE($4, visibility)
+        WHERE id = $1`
+        ,[id,name, description, visibility]
+    );
+    
+    return `Repo updated successfully.`;
+}
+
+const _delete = async ({id})=>{
+    const repos = await query(
+        `DELETE FROM Repos
+        WHERE id = $1`
+        ,[id]
+    );
+    return `Repo deleted successfully.`;
+
 }
 
 module.exports = {
     queryAllRepos,
     createRepo,
-    queryAllReposOfUser
+    queryAllReposOfUser,
+    queryRepoNameOfUser,
+    update,
+    _delete
 }
