3ba477f264381b1a74e7d85a6b4f18c9d722038f
SaadAliBaig84 <mirzasaad363@gmail.com>
2024-10-10 07:15:59 -0700

    saad's second

--- Backend/controllers/repoController.js
+++ Backend/controllers/repoController.js
@@ -20,7 +20,7 @@ const createRepo = async (req, res) => {
         const user_id = req.user;
         const validated_params = validateRepoParams(user_id, req.body, 'c');
 
-        const repos = await repoQuery.queryAllRepos();
+        const repos = await repoQuery.queryAllReposOfUser(user_id);
         const names = repos.map(data => data.name);
         
         if(names && names.includes(validated_params.name)){
@@ -55,7 +55,7 @@ const updateRepo =async (req, res)=>{
     try {
         const user_id=req.user;
         const validated_params = validateRepoParams(user_id, req.body, 'u');
-        const repo = await repoQuery.queryAllRepos();
+        const repo = await repoQuery.queryAllReposOfUser(user_id);
         const ids = repo.map(data => data.id);
         const names = repo.map(data=>data.name);
         const currName=await repoQuery.queryRepoNameOfUser( req.body.id);
@@ -78,9 +78,9 @@ const updateRepo =async (req, res)=>{
 
 const deleteRepo = async (req, res)=>{
     try {
-        const repo = await repoQuery.queryAllRepos();
-        const ids = repo.map(data => data.id);
         const user_id=req.user;
+        const repo = await repoQuery.queryAllReposOfUser(user_id);
+        const ids = repo.map(data => data.id);
         const validated_params = validateRepoParams(user_id,req.body, 'd');
         if(!ids.includes(req.body.id)){
             throw Error(`There is no repo with id  "${req.body.id}".`);
