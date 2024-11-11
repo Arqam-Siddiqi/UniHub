b7c8ad2ef878b425a1bf02d433404513c2256a2d
Arqam-Siddiqi <siddiqi.seerow@gmail.com>
2024-11-11 22:12:15 +0500

    Minor changes in the commentRoutes.js file.

--- Backend/routes/commentRoutes.js
+++ Backend/routes/commentRoutes.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
 
-const commentController=require('../controllers/commentController');
+const commentController = require('../controllers/commentController');
 
 router.get('/', commentController.getAllComments);
-router.get('/selfuser', commentController.getAllUserComments)
-router.get('/selfrepo', commentController.getAllRepoComments)
-router.post('/create',commentController.createComment );
+
+router.get('/self/user', commentController.getAllUserComments);
+
+router.post('/self/repo', commentController.getAllRepoComments);
+
+router.post('/create',commentController.createComment);
+
 router.patch('/update', commentController.updateComment);
+
 router.delete('/delete', commentController.deleteComment);
-module.exports=router;
+
+module.exports = router;
