fd5b07f10bd7b05372d6c59e81e11f885aa35b9e
SaadAliBaig84 <mirzasaad363@gmail.com>
2024-11-11 11:48:58 -0800

    Mussab, comments done

--- Backend/controllers/commentController.js
+++ Backend/controllers/commentController.js
@@ -1,6 +1,26 @@
 const {validateCommentParams} =  require('../utils/commentUtils');
 const commentQuery=require('../database/commentQuery');
-
+const userQuery = require('../database/userQuery');
+const getUser = async (comment, user_id)=>{
+    const user= await userQuery.queryUserByID(user_id);
+    const user_name=user.name;
+    
+    const email=user.email;
+    if(Array.isArray(comment))
+    for(i=0; i<comment.length; i++){
+        comment[i].name=user_name;
+        comment[i].email=email;
+    }
+    else
+    {
+        comment.name=user_name;
+        comment.email=email;
+    }
+    
+        
+    
+    
+}
 const getAllComments = async(req, res)=>{
     try{
         const comments = await commentQuery.queryAllComments();
@@ -15,7 +35,12 @@ const getAllComments = async(req, res)=>{
 const getAllUserComments = async(req, res)=>{
     try{
         const comments = await commentQuery.queryByUser(req.user);
-
+        await getUser(comments, req.user);
+        // const user= await userQuery.queryUserByID(req.user);
+        // const user_name=user.name;
+        // const email=user.email;
+        // const details={user_name,email};
+        // res.status(200).send({details,comments});
         res.status(200).send(comments);
     }
     catch(error){
@@ -29,6 +54,7 @@ const getAllRepoComments = async(req, res)=>{
             throw Error('Give a repo id');
         }
         const comments = await commentQuery.queryByRepo(req.body.repo_id);
+        await getUser(comments, req.user);
 
         res.status(200).send(comments);
     }
@@ -43,7 +69,12 @@ const createComment = async (req, res) => {
         
         const validated_params = validateCommentParams(user_id,req.body, 'c');
         const comment=await commentQuery.comment(user_id,validated_params.repo_id, validated_params.content);
-        
+        // const user= await userQuery.queryUserByID(user_id);
+        // const user_name=user.name;
+        // comment.name=user_name;
+        // const email=user.email;
+        // comment.email=email;
+        await getUser(comment, user_id);
         
         res.status(200).send(comment);
     }
@@ -64,6 +95,7 @@ const updateComment =async(req,res)=>{
             throw Error('The comment was not made by this user');
         }
         const comment=await commentQuery.update(validated_params.id, validated_params.content); 
+        await getUser(comment,user_id);
         res.status(200).send(comment);
     }
     catch(error){
@@ -82,6 +114,7 @@ const deleteComment = async (req,res)=>{
             throw Error('No such comment exists for this user');
         }
         const comment=await commentQuery._delete(validated_params.id); 
+        await getUser(comment, user_id);
         res.status(200).send(comment);
     }
     catch(error){
