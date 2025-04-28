212489c8a85bde606a7dec47bf387d89e31d4281
Arqam-Siddiqi <113540979+Arqam-Siddiqi@users.noreply.github.com>
2025-04-29 00:59:47 +0500

    Patch: Updated Gemini_1.5-Flash to Gemini_2.5-Flash

--- Backend/utils/geminiUtils.js
+++ Backend/utils/geminiUtils.js
@@ -10,7 +10,7 @@ const initializeGemini = (maxOutputTokens) => {
     const fileManager = new GoogleAIFileManager(process.env.GEMINI_KEY);
 
     const model = genAI.getGenerativeModel({
-        model: "gemini-1.5-flash",
+        model: "gemini-2.5-flash",
         generationConfig: {
             temperature: 0.2,
             responseMimeType: 'application/json',
